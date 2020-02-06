10704f00ae3399b9a47b00ff3e4376e73c6a2bb0
Hauk Olaussen <olaussen@users.noreply.github.com>
2020-02-06 14:34:08 +0100

    kosebloggen adds

--- kosebloggen/blog/models.py
+++ kosebloggen/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to="gallery")
+
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
