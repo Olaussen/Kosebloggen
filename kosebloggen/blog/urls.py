ee10a07f44e381323a4166fc9294c8835539f378
haukao <Olaussen99>
2020-02-11 22:00:17 +0100

    arbeidsdag 2

--- kosebloggen/blog/urls.py
+++ kosebloggen/blog/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('/om', views.AboutView.as_view(), name="about"),
+    path('/kontakt', views.ContactView.as_view(), name="contact"),
 ]
--- kosebloggen/blog/views.py
+++ kosebloggen/blog/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+import random
 
 class PostList(generic.ListView):
     queryset = Post.objects.order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'blog/index.html'
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['extra_list'] = set(random.choices(Post.objects.exclude(slug = Post.slug), k=3))
+        return context
+
+class AboutView(generic.TemplateView):
+    template_name = 'blog/about.html'
+
+class ContactView(generic.TemplateView):
+    template_name = 'blog/contact.html'
+
 
